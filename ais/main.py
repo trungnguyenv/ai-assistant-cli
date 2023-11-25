b608f7a6843c75442c8459c90752257d4a871c0d
Trung Nguyen <nguyenviettrung1995@gmail.com>
2023-11-25 20:22:08 +0700

    Allow adjustments

--- ais/main.py
+++ ais/main.py
@@ -1,12 +1,12 @@
-import json
 import subprocess
 
 import typer
 from openai import OpenAI
+from rich.prompt import Prompt
 
+import ais.auth_app
 from ais.api_key_providers import KeyRingProvider
 from ais.utils import get_system_info
-import ais.auth_app
 
 app = typer.Typer()
 app.add_typer(ais.auth_app.app)
@@ -15,14 +15,10 @@ key_provider = KeyRingProvider()
 
 @app.command()
 def cmd(prompt: str):
-    client = OpenAI(api_key=key_provider.get_api_key())
-
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": f"""You are AI Command Line Assistant, your task is to help to turn user input into a 
+    messages = [
+        {
+            "role": "system",
+            "content": f"""You are AI Command Line Assistant, your task is to help to turn user input into a 
 valid command line for execution Make sure the command use the correct syntax of the provided system 
 info
 Only output the command, do not explain
@@ -31,23 +27,69 @@ The command must be oneliner
 System Info: 
 {get_system_info()}`
 """
-            },
-            {
+        },
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+
+    response = get_ai_completion(messages)
+
+    command = response.choices[0].message.content
+    typer.echo(f"The suggested command is: {typer.style(command, fg=typer.colors.GREEN)}")
+
+    prompt_execute = typer.style("E", fg=typer.colors.GREEN, bold=True) + "xecute"
+    prompt_adjust = typer.style("A", fg=typer.colors.YELLOW, bold=True) + "djust"
+    prompt_cancel = typer.style("C", fg=typer.colors.RED, bold=True) + "ancel"
+
+    choice = Prompt.ask(f"Choose action to perform ({prompt_execute}/{prompt_adjust}/{prompt_cancel})",
+                        choices=["E", "A", "C"], default="C")
+
+    while choice == "A":
+        if choice == "A":
+            adjustment = Prompt.ask("What to adjust?")
+            messages.append(response.choices[0].message)
+            messages.append({
                 "role": "user",
-                "content": prompt
-            }
-        ],
+                "content": adjustment
+            })
+            response = get_ai_completion(messages)
+
+            command = response.choices[0].message.content
+            typer.echo(f"The suggested command is: {typer.style(command, fg=typer.colors.GREEN)}")
+
+            choice = Prompt.ask(f"Choose action to perform ({prompt_execute}/{prompt_adjust}/{prompt_cancel})",
+                                choices=["E", "A", "C"], default="C")
+
+    if choice == "E":
+        try:
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                       text=True)
+            output, error = process.communicate()
+
+            if process.returncode != 0:
+                typer.echo(f"Error: {error}", err=True)
+            else:
+                typer.echo(output)
+
+        except subprocess.SubprocessError as e:
+            typer.echo(f"An error occurred: {e}", err=True)
+
+
+def get_ai_completion(messages):
+    client = OpenAI(api_key=key_provider.get_api_key())
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
         temperature=0,
         max_tokens=1024,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
     )
-
-    command = response.choices[0].message.content
-    typer.echo(f"The suggested command is: {typer.style(command, fg=typer.colors.GREEN)}")
-    if typer.confirm("Do you want to execute it?"):
-        typer.echo(subprocess.check_output(command, shell=True, text=True))
+    return response
 
 
 if __name__ == "__main__":
